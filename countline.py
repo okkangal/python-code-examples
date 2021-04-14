a921db056d2300ea2c5ff44bba7fd28c2830fd4e
Okay KANGAL <okaykangal@gmail.com>
2021-04-14 17:03:22 +0300

    Create countline.py

---
+++ countline.py
@@ -0,0 +1,10 @@
+# count the lines of a file 
+
+
+fname = input("Enter file name: ")
+num_lines = 0
+with open(fname, 'r') as f:
+    for line in f:
+        num_lines += 1
+print("Number of lines:")
+print(num_lines
