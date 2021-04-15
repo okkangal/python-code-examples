cfc743cac86d67d4f3b6e748e8a51de21770e9d9
Okay KANGAL <okaykangal@gmail.com>
2021-04-16 00:42:34 +0300

    Create leftrightmiddle.py

---
+++ leftrightmiddle.py
@@ -0,0 +1,8 @@
+>>> s = "|{:<10}|{:^10}|{:>10}|".format("Hale", "Lale","Jale")    # It is understood after watching for a while :))) 
+>>> s
+'|Hale      |   Lale   |      Jale|'
+
+
+
+
+>>>   < left  ^ Middle  > Right     
