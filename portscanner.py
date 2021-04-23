3f6e7bcf1babc037d2fbd08ffd2bc18a43e535e2
Okay KANGAL <okaykangal@gmail.com>
2021-04-23 21:36:14 +0300

    Create portscanner.py

---
+++ portscanner.py
@@ -0,0 +1,9 @@
+import socket
+
+for port in range(1, 1025):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+    result = sock.connect_ex(("192.168.0.1", port))
+    if 0 == result:
+        print("Port: {} Open".format(port))
+    sock.close()
